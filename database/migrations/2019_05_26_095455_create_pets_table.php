9a0dd0b6fee1b85ca14c4c0ad51c5780f2844fbf
Justin Liu <justinjiahaoliu@gmail.com>
2019-05-26 20:11:53 +1000

    Add pet table

---
+++ database/migrations/2019_05_26_095455_create_pets_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pets', function (Blueprint $table) {
+            $table->bigIncrements('petId');      
+            $table->string('petName');       
+            $table->string('species');       
+            $table->string('breed');        
+            $table->date('DOB');       
+            $table->char('gender', 1);       
+            $table->integer('weight');
+            $table->bigInteger('customerId');
+            $table->timestamps();
+            $table->foreign('customerId')->references('customerId')->on('customers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pets');
+    }
+}
